c4e2eadfa21ea8001d7b344bf88d875ddece1638
B Kleinen <kleinen@infrastructure.de>
2016-01-27 23:52:03 +0100

    cleanup

---
+++ address-book-v3t/__SHELL1.java
@@ -0,0 +1,10 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/kleinen/mine/htw/courses/info1/code/class/bluej/chapter12-error-handling/address-book-v3t");
+final AddressBookDemo addressB1 = (AddressBookDemo)__bluej_runtime_scope.get("addressB1");
+
+
+addressB1.showInterface();
+
+}}
