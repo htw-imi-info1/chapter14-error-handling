05d9f0d50e99393ce019c4b2861f55a58d009497
Barne Kleinen <barne.kleinen@htw-berlin.de>
2024-01-09 13:00:59 +0100

    add exception playground

---
+++ exeptions-errors/Playground.java
@@ -0,0 +1,33 @@
+
+public class Playground
+{
+    public static void main(String[] args){
+        int a = Integer.valueOf(args[0]);
+        int b = Integer.valueOf(args[1]);
+
+        try
+        {
+            int result = new Playground().division(a,b);
+            System.out.println("the result is: "+result);
+        }
+        catch (Exception e)
+        {
+            System.out.println("There was a problem: "+e.getMessage());
+            //e.printStackTrace();
+        }
+
+       
+    }
+
+    public static void run() throws Exception {
+        new Playground().division(7,0);
+    }
+
+    public int division(int a, int b) throws Exception {
+        if (b == 0){
+           // System.out.println("b darf nicht null sein");
+            throw new Exception("b darf nicht null sein");
+        }
+        return a/b;
+    }
+}
